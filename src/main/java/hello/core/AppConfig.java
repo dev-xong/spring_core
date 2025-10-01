1bb1e5a18d0a39394a6558d38ba9c5effd4ae8a5
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-10-02 00:03:49 +0900

    21. 새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.memeber.MemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
@@ -28,6 +29,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,7 +18,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 1000);
+        Order order = orderService.createOrder(memberId, "itemA", 24000);
 
         System.out.println("order : " + order);
 
